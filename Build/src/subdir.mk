90816d53582fd9dd71685f5f3a3518ce3f81621b
crinders <crinders@gmail.com>
2018-04-25 09:15:38 -0400

    post add edits not commited

--- Build/src/subdir.mk
+++ Build/src/subdir.mk
@@ -1,7 +1,3 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
 # Add inputs and outputs from these tool invocations to the build variables 
 CC_SRCS += \
 ../src/DiscreteCCSOpt.cc \
@@ -61,7 +57,7 @@ DOBJS += \
 src/%.o: ../src/%.cc
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	$(CXX) -fpermissive -I$(HOME)/projects/tmp -I.. -I../include -I../src -I"../" -O3 -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	$(CXX) -fpermissive -I$(BCRCPPLAROOT) -I.. -I../include -I../src -I"../" -O3 -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
