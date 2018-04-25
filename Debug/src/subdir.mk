7a3e71c6a91ed702deb2dc80cbb8adc0bf796b53
crinders <crinders@gmail.com>
2018-04-25 09:14:18 -0400

    Same as Build with Debug

--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -1,7 +1,3 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
 # Add inputs and outputs from these tool invocations to the build variables 
 CC_SRCS += \
 ../src/DiscreteCCSOpt.cc \
@@ -61,7 +57,7 @@ DOBJS += \
 src/%.o: ../src/%.cc
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	/usr/local/bin/g++ -g -DDEBUG -fpermissive -I/usr/local/include -I/path/to/BCR_CPP_LA -I"../include" -I"../src" -I"../" -O3 -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	$(CXX) -g -DDEBUG -fpermissive -I/usr/local/include -I$(BCRCPPLAROOT) -I"../include" -I"../src" -I"../" -O3 -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
